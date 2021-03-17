13e3d0c04d3e724688bd112212dd174c7162f529
91800 <91800@LAPTOP-UE1S6U11>
2021-03-17 12:13:26 +0530

    for example “” or “1” or “1,2” as inputs <<--Condotion SuccessFull

--- Calculator/src/com/test/CalculatorTest.java
+++ Calculator/src/com/test/CalculatorTest.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 
-	
 	public int add(String data) {
-		int sum=0;
+		int sum = 0;
 		System.out.println(data);
-		
+		for(int i=0; i<data.length();i++) {
+			char temp = data.charAt(i);
+			if(Character.isDigit(temp)) {
+				int number = Integer.parseInt(String.valueOf(temp));
+				sum = sum + number;
+				
+			}
+		}
 		return sum;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		CalculatorTest test = new CalculatorTest();
 		Scanner sc = new Scanner(System.in);
