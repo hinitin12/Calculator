a0df681a17a6ee82ba9c01a54ecca54907bcb8c5
91800 <91800@LAPTOP-UE1S6U11>
2021-03-17 12:26:22 +0530

    Custom Exception Throws for Negative Number and Assignment Done.

--- Calculator/src/com/test/CalculatorTest.java
+++ Calculator/src/com/test/CalculatorTest.java
@@ -7,22 +7,40 @@ public class CalculatorTest {
 	public int add(String data) {
 		int sum = 0;
 		System.out.println(data);
-		for(int i=0; i<data.length();i++) {
-			char temp = data.charAt(i);
-			if(Character.isDigit(temp)) {
-				int number = Integer.parseInt(String.valueOf(temp));
-				sum = sum + number;
-				
+		for (int i = 0; i < data.length(); i++) {
+			try {
+				if (data.charAt(i) == '-') {
+					throw new NegativeNumberException("Negative Number Exception");
+					// System.out.println("Negative Number Exception");
+				} else {
+
+					char temp = data.charAt(i);
+					if (Character.isDigit(temp)) {
+						int number = Integer.parseInt(String.valueOf(temp));
+						sum = sum + number;
+
+					}
+				}
+			} catch (NegativeNumberException ne) {
+				ne.printStackTrace();
 			}
 		}
 		return sum;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NegativeNumberException {
 		CalculatorTest test = new CalculatorTest();
 		Scanner sc = new Scanner(System.in);
 		int input = test.add(sc.next());
-		System.out.println(input);
+		System.out.println("Addition : "+input);
+	}
+
+}
+
+class NegativeNumberException extends Exception {
+
+	public NegativeNumberException(String s) {
+		super(s);
 	}
 
 }
