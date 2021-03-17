fa0163d2caaf84d4140d66766585086de33955b0
91800 <91800@LAPTOP-UE1S6U11>
2021-03-17 12:00:25 +0530

    Add Function Created And Pass the input From User

--- Calculator/src/com/test/CalculatorTest.java
+++ Calculator/src/com/test/CalculatorTest.java
@@ -1,10 +1,23 @@
 package com.test;
 
+import java.util.Scanner;
+
 public class CalculatorTest {
 
+	
+	public int add(String data) {
+		int sum=0;
+		System.out.println(data);
+		
+		return sum;
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CalculatorTest test = new CalculatorTest();
+		Scanner sc = new Scanner(System.in);
+		int input = test.add(sc.next());
+		System.out.println(input);
 	}
 
 }
